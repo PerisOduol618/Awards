ec52ca919da142b46f45f767464306850ed4281e
PerisOduol618 <oduolpepe618@gmail.com>
2020-10-26 01:36:19 +0300

    define a view function for the new projects

--- rates/views.py
+++ rates/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from .forms import Project, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
+from .models import Profile, Post
+
 
 @login_required(login_url='/accounts/login/')
 def home(request):
@@ -75,3 +77,19 @@ def update_profile(request):
 
 
 @login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+   
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+           
+            image.save()
+            
+        return redirect('home')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {"form": form})
